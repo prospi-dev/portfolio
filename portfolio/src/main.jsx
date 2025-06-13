1d90900c14e91b28339f34592b47ae11012347ea
Prospi <manuprosperi04@gmail.com>
2025-06-13 15:10:56 +0000

    Base code added

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 const App = () => {
+  return (
+    <div>
+
+    </div>
+  )
 };
 
 createRoot(document.getElementById('root')).render(<App />);
